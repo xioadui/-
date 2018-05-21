b61d956c2e417ceaf49f56edba3df29fe7fc0a2e
MyTestClass <39087163+MyTestClass@users.noreply.github.com>
2018-05-21 21:06:41 +0800

    ee

--- src/main/java/com/enterprise/dao/IInformationDao.java
+++ src/main/java/com/enterprise/dao/IInformationDao.java
@@ -10,10 +10,14 @@ import java.util.List;
 public interface IInformationDao {
 //    保存到数据库中
     public void save(@Param("info") List<Information> info);
-//    通过标题查询
-    public List<Information> queryByTitle(String title);
+//    通过标题查询  index是从什么位置开始 length是要查询数据的长度
+    public List<Information> queryByTitle(@Param("title")String title);
 //    通过资讯的时间查询
-    public List<Information> queryByDate(String date);
-    //获取所有资讯
-    List<Information> getAll();
+    public List<Information> queryByDate(@Param("date")String date ,
+                                         @Param("index")long index,
+                                         @Param("length")int length);
+//    通过行业进行查询
+    public List<Information> queryByType(@Param("type")String type,
+                                         @Param("index")long index,
+                                         @Param("length")int length);
 }
