4b84aa85e54a4457a63fae697b9bf10807226af5
LinA1806 <834183389@qq.com>
2018-06-02 19:27:50 +0800

    主页和供需详情页改好啦
    没登陆的时候点首页的浏览供需/发布供需/合作伙伴都是跳到登录页，但是登录页还没有发上来就先发一个空白页大家自行感受一下~

--- 其它/企业用户/js/index.js
+++ 其它/企业用户/js/index.js
@@ -5,12 +5,22 @@ $(".guide li").mouseenter(function() {
 			}
 		})
 
-$(".fun_list li").mouseenter(function() {
+$(".func1 li").mouseenter(function() {
 			if(!$(this).hasClass(".active")) {
 				$(".active").removeClass("active");
 				$(this).addClass("active");
-				$(".cont div").removeClass("showed");
-				$(".cont div").eq($(this).index()).addClass("showed");
+				$("#cont1 div").removeClass("showed");
+				$("#cont1 div").eq($(this).index()).addClass("showed");
+				
+			}
+		})
+
+$(".func2 li").mouseenter(function() {
+			if(!$(this).hasClass(".active")) {
+				$(".active").removeClass("active");
+				$(this).addClass("active");
+				$("#cont2 div").removeClass("showed");
+				$("#cont2 div").eq($(this).index()).addClass("showed");
 				
 			}
 		})
@@ -20,6 +30,80 @@ $(document).ready(function(){
     div.animate({fontSize:'17px'},"slow");
 });
 
-$(".info").hover(function() {
-	$(".info h3").animate({fontSize:'5vw'},"fast");
+$(document).ready(function() {
+	$.get(URL,  {
+					category: '医疗医药保健',
+				},
+				function(data) {
+						$('#cont1 #tab1 h4').append(infTitle);/*获得资讯名称*/
+						$('#cont1 #tab1 p').append(infDigeat+'...');/*获得资讯摘要*/
+						$('#cont2 #tab1 h4').append(indTitle);/*获得行业数据名称*/
+						$('#cont2 #tab1 p').append(infDigeat+'...');/*获得行业数据摘要*/
+					})
+	$.get(URL,  {
+					category: 'IT与通讯',
+				},
+				function(data) {
+						$('#cont1 #tab2 h4').append(infTitle);/*获得资讯名称*/
+						$('#cont1 #tab2 p').append(infDigeat+'...');/*获得资讯摘要*/
+						$('#cont2 #tab2 h4').append(indTitle);/*获得行业数据名称*/
+						$('#cont2 #tab2 p').append(infDigeat+'...');/*获得行业数据摘要*/
+					})
+	$.get(URL,  {
+					category: '机械电子',
+				},
+				function(data) {
+						$('#cont1 #tab3 h4').append(infTitle);/*获得资讯名称*/
+						$('#cont1 #tab3 p').append(infDigeat+'...');/*获得资讯摘要*/
+						$('#cont2 #tab3 h4').append(indTitle);/*获得行业数据名称*/
+						$('#cont2 #tab3 p').append(infDigeat+'...');/*获得行业数据摘要*/
+					})
+	$.get(URL,  {
+					category: '车辆交通',
+				},
+				function(data) {
+						$('#cont1 #tab4 h4').append(infTitle);/*获得资讯名称*/
+						$('#cont1 #tab4 p').append(infDigeat+'...');/*获得资讯摘要*/
+						$('#cont2 #tab4 h4').append(indTitle);/*获得行业数据名称*/
+						$('#cont2 #tab4 p').append(infDigeat+'...');/*获得行业数据摘要*/
+					})
+	$.get(URL,  {
+					category: '能源矿产',
+				},
+				function(data) {
+						$('#cont1 #tab5 h4').append(infTitle);/*获得资讯名称*/
+						$('#cont1 #tab5 p').append(infDigeat+'...');/*获得资讯摘要*/
+						$('#cont2 #tab5 h4').append(indTitle);/*获得行业数据名称*/
+						$('#cont2 #tab5 p').append(infDigeat+'...');/*获得行业数据摘要*/
+					})
 });
+
+$(document).ready(function(){
+//	如果用户已登录,将span标签加入welcome类
+//隐藏登陆/注册按钮
+//显示欢迎信息
+//	浏览供需/发布供需/合作伙伴可正常跳转至相应的页面
+//	否则跳转至登录页
+   if($(".guide span").hasClass("welcome")) {	
+   	$("#login").addClass("hidden");
+	$("#register").addClass("hidden");
+	$("#findsr").append('<a href="search_sup&req.html">浏览供需</a>');
+	$("#sr").append('<a href="sup&req.html">发布供需</a>');
+	$("#partner").append('<a href="find_partner.html">合作伙伴</a>');
+   	$.get(URL, function(data) {
+					$(".guide span").append(entName+'，欢迎您！');
+				})
+			}
+   else{
+   	$("#findsr").append('<a href="login.html">浏览供需</a>');
+	$("#sr").append('<a href="login.html">发布供需</a>');
+   	$("#partner").append('<a href="login.html">合作伙伴</a>');
+   }
+});
+
+$(".guide span").mouseenter(function() {
+			if(!$(this).hasClass(".active")) {
+				$(".active").removeClass("active");
+				$(this).addClass("active");
+			}
+		})
--- 其它/企业用户/js/read_sup&req.js
+++ 其它/企业用户/js/read_sup&req.js
@@ -20,6 +20,26 @@ $(document).ready(function() {
 					})
 });
 
+$(document).ready(function() {
+	$.get(URL, function(data) {
+						$('.comment_message').append('<nav class="someone"><p class="some_name">' + entName+'</p>');/*获得评论者名称*/
+						$('.comment_message').append('<span class="some_time">'+commentDate+'</span>');/*获得留言时间*/
+						$('.comment_message').append('<p class="some_message">'+commentContent+'</p>	</nav>');/*获得留言内容*/
+					})
+});
+
+$(document).ready(function() {
+	$.get(URL, function(data) {
+						$('.name').append(entName);/*获得企业名称*/
+						$('.phone').append(entPhone);/*获得电话*/
+						$('.mail').append(entID);/*获得企业邮箱*/
+						$('.address').append(entAddress);/*获得企业地址*/
+						$('.link').append(entWebsize);/*获得企业网址*//*Website？*/
+						$('.synopsis').append(entBreif);/*获得企业简介*//*Website？*/
+						$('.check').append('<a href="'+lll +'>查看企业</a>' );
+					})
+});
+
 $(".sub").on("click", function() {
 	$.post('url', {
 			comment: $("#con").val(),
