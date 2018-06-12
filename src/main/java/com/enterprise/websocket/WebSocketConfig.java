8d32a02fded1bc1e3873351dd27b9f7470c3e00e
MyTestClass <39087163+MyTestClass@users.noreply.github.com>
2018-06-12 20:34:03 +0800

    1

--- src/main/java/com/enterprise/websocket/WebSocketConfig.java
+++ src/main/java/com/enterprise/websocket/WebSocketConfig.java
@@ -1,28 +1,25 @@
 package com.enterprise.websocket;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
-import org.springframework.web.socket.server.HandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(chatMessageHandler(),"/message").setAllowedOrigins("*").addInterceptors(chatHandlerShake());
+        webSocketHandlerRegistry.addHandler(chatMessageHandler(),"/message").setAllowedOrigins("*");//.addInterceptors(chatHandlerShake());
     }
     @Bean
     public AbstractWebSocketHandler chatMessageHandler(){
         return new ChatHandler();
     }
-    @Bean
-    public HandshakeInterceptor chatHandlerShake(){
-        return new ChatHandlerShake();
-    }
+//    @Bean
+//    public HandshakeInterceptor chatHandlerShake(){
+//        return new ChatHandlerShake();
+//    }
 }
