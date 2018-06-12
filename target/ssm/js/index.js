48fb19700da629877a9874fc52d99b525e37bc02
MyTestClass <39087163+MyTestClass@users.noreply.github.com>
2018-06-13 00:18:48 +0800

    1

--- target/ssm/js/index.js
+++ target/ssm/js/index.js
@@ -1,25 +1,4 @@
-$(document).ready(function () {
-//	如果用户已登录,将span标签加入welcome类
-//隐藏登陆/注册按钮
-//显示欢迎信息
-//	浏览供需/发布供需/合作伙伴可正常跳转至相应的页面
-//	否则跳转至登录页
-    if ($(".guide span").hasClass("welcome")) {
-        $("#login").addClass("hidden");
-        $("#register").addClass("hidden");
-        $("#findsr").append('<a href="search_sup&req.html">浏览供需</a>');
-        $("#sr").append('<a href="sup&req.html">发布供需</a>');
-        $("#partner").append('<a href="find_partner.html">合作伙伴</a>');
-        $.get(URL, function (data) {
-            $(".guide span").append(entName + '，欢迎您！');
-        })
-    }
-    else {
-        $("#findsr").append('<a href="login.html">浏览供需</a>');
-        $("#sr").append('<a href="login.html">发布供需</a>');
-        $("#partner").append('<a href="login.html">合作伙伴</a>');
-    }
-});
+
 
 $(".guide span").mouseenter(function () {
     if (!$(this).hasClass(".active")) {
@@ -163,42 +142,42 @@ $(document).ready(function () {
 
 });
 
-// $(document).ready(function() {
-// 	//	如果用户已登录,在span标签加入welcome类
-// 	//隐藏登陆/注册按钮
-// 	//显示欢迎信息
-// 	//	浏览供需/发布供需/合作伙伴可正常跳转至相应的页面
-// 	//	否则跳转至登录页
-// 	if($(".guide span").hasClass("welcome")) {
-// 		$("#login").addClass("hidden");
-// 		$("#register").addClass("hidden");
-// 		$("#findsr").append('<a href="search_sup&req.html">浏览供需</a>');
-// 		$("#sr").append('<a href="sup&req.html">发布供需</a>');
-// 		$("#partner").append('<a href="find_partner.html">合作伙伴</a>');
-//
-// 		$.ajax({
-// 			type: "get",
-// 			url: "", //企业表
-// 			dataType: "json",
-// 			async: false,
-// 			success: function(data) {
-// 				$(".guide span").append(data.entName + '，欢迎您！');
-// 			},
-// 			error: function(error) {
-// 				console.log(error);
-// 				//			alert("异常！");
-// 			}
-// 		});
-// 	} else {
-// 		$("#findsr").append('<a href="login.html">浏览供需</a>');
-// 		$("#sr").append('<a href="login.html">发布供需</a>');
-// 		$("#partner").append('<a href="login.html">合作伙伴</a>');
-// 	}
-// });
-//
-// $(".guide span").mouseenter(function() {
-// 	if(!$(this).hasClass(".active")) {
-// 		$(".active").removeClass("active");
-// 		$(this).addClass("active");
-// 	}
-// })
+$(document).ready(function() {
+	//	如果用户已登录,在span标签加入welcome类
+	//隐藏登陆/注册按钮
+	//显示欢迎信息
+	//	浏览供需/发布供需/合作伙伴可正常跳转至相应的页面
+	//	否则跳转至登录页
+	if($(".guide span").hasClass("welcome")) {
+		$("#login").addClass("hidden");
+		$("#register").addClass("hidden");
+		$("#findsr").append('<a href="search_sup&req.html">浏览供需</a>');
+		$("#sr").append('<a href="sup&req.html">发布供需</a>');
+		$("#partner").append('<a href="find_partner.html">合作伙伴</a>');
+
+		$.ajax({
+			type: "get",
+			url: "", //企业表
+			dataType: "json",
+			async: false,
+			success: function(data) {
+				$(".guide span").append(data.entName + '，欢迎您！');
+			},
+			error: function(error) {
+				console.log(error);
+				//			alert("异常！");
+			}
+		});
+	} else {
+		$("#findsr").append('<a href="login.html">浏览供需</a>');
+		$("#sr").append('<a href="login.html">发布供需</a>');
+		$("#partner").append('<a href="login.html">合作伙伴</a>');
+	}
+});
+
+$(".guide span").mouseenter(function() {
+	if(!$(this).hasClass(".active")) {
+		$(".active").removeClass("active");
+		$(this).addClass("active");
+	}
+})
