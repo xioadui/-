e695b3555f78e43cc45603e8a5625a899242ec38
LinA1806 <834183389@qq.com>
2018-06-13 09:20:17 +0800

    资讯详情页和行业数据详情页

---
+++ 其它/企业用户/js/news.js
@@ -0,0 +1,22 @@
+$(document).ready(function){
+	var name = $.query.get("infTitle");//获得资讯标题
+	$.ajax({
+			type: "get",
+			url: "", //资讯表
+			dataType: "json",
+			data:{
+				infTitle: name;
+			},
+			async: false,
+			success: function(data) {
+				$(".news_content").empty();
+				$(".news_content h1").append('<h1>'+data.infTitle+'</h1>');
+				$(".news_content h1").append('<nav><span>'+data.infType+'</span><span>' +data.infDate+'</span></nav>');
+				$(".news_content h1").append('<div>'+data.infContent+'</div>');
+			},
+			error: function(error) {
+				console.log(error);
+				//			alert("异常！");
+			}
+		});
+}
